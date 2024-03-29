2b752919fa0a51181beb6c08e7bd1b07a316ec9b
fescii <isfescii@gmail.com>
2024-03-29 15:16:11 +0300

    Opinion: Entry  A102

--- js/containers/trending.container.js
+++ js/containers/trending.container.js
@@ -48,12 +48,10 @@ export default class TrendingContainer extends HTMLElement {
     return `
 			<div class="stories new">
         <div class="title">
-          <span class="icon">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-quote" viewBox="0 0 16 16">
-              <path d="M12 12a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1h-1.388q0-.527.062-1.054.093-.558.31-.992t.559-.683q.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 9 7.558V11a1 1 0 0 0 1 1zm-6 0a1 1 0 0 0 1-1V8.558a1 1 0 0 0-1-1H4.612q0-.527.062-1.054.094-.558.31-.992.217-.434.559-.683.34-.279.868-.279V3q-.868 0-1.52.372a3.3 3.3 0 0 0-1.085.992 4.9 4.9 0 0 0-.62 1.458A7.7 7.7 0 0 0 3 7.558V11a1 1 0 0 0 1 1z"/>
-            </svg>
+          <h2>${this.getAttribute('topic')}</h2>
+          <span class="info">
+            60 new stories
           </span>
-          <h2>${this.getAttribute('topic')}
         </div>
 				${this.getLoader()}
       </div>
@@ -162,6 +160,45 @@ export default class TrendingContainer extends HTMLElement {
 			  width: 100%;
 			  display: flex;
 			  flex-flow: column;
+			  gap: 0;
+			}
+
+      div.stories > .title {
+			  /* border: 1px solid #000000; */
+			  padding: 0;
+			  width: 100%;
+			  display: flex;
+			  flex-flow: row;
+        color: var(--text-color);
+			  gap: 2px;
+			}
+
+      div.stories > .title > .icon {
+			  /* border: 1px solid #000000; */
+			  padding: 0;
+			  display: flex;
+        color: var(--gray-color);
+			  gap: 0;
+			}
+
+      div.stories > .title > .icon > svg {
+			  /* border: 1px solid #000000; */
+			  padding: 0;
+        margin: 0 0 -2px -3px;
+			  display: flex;
+        color: var(--gray-color);
+			  gap: 0;
+			}
+
+      div.stories > .title > h2 {
+			  /* border: 1px solid #000000; */
+        text-transform: capitalize;
+			  padding: 0;
+        margin: 0;
+        font-weight: 500;
+        font-size: 1.5rem;
+        font-family: var(--font-main), sans-serif;
+        color: var(--text-color);
 			  gap: 0;
 			}
 
