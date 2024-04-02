ba89fa4d5f0a166f86cb63bd0263b4e63a4da632
fescii <isfescii@gmail.com>
2024-04-02 12:08:02 +0300

    Profile: Entry  A40

--- js/story.component.js
+++ js/story.component.js
@@ -577,7 +577,7 @@ export default class StoryWrapper extends HTMLElement {
         display: flex;
       }
 
-      .meta  .profile > span.pointer {
+      .meta .profile > span.pointer {
         border: var(--modal-border);
         border-bottom: none;
         border-right: none;
@@ -594,12 +594,12 @@ export default class StoryWrapper extends HTMLElement {
         -moz-border-radius: 1px;
       }
 
-      .meta.opinion  .profile > span.pointer{
+      .meta.opinion .profile > span.pointer{
         left: unset;
-        right: 50%;
+        right: 45%;
       }
 
-      .meta  .profile > .cover>  .head {
+      .meta .profile > .cover > .head {
         background-color: var(--background);
         display: flex;
         flex-wrap: nowrap;
@@ -607,7 +607,7 @@ export default class StoryWrapper extends HTMLElement {
         gap: 10px;
       }
 
-      .meta  .profile > .cover>  .head > .image {
+      .meta .profile > .cover > .head > .image {
         width: 40px;
         height: 40px;
         overflow: hidden;
@@ -616,7 +616,7 @@ export default class StoryWrapper extends HTMLElement {
         -moz-border-radius: 50px;
       }
 
-      .meta  .profile > .cover>  .head > .image img {
+      .meta .profile > .cover > .head > .image img {
         width: 100%;
         height: 100%;
         object-fit: cover;
@@ -702,12 +702,14 @@ export default class StoryWrapper extends HTMLElement {
         -webkit-border-radius: 8px;
         -moz-border-radius: 8px;
       }
+
       .meta span.action.follow {
         border: none;
         text-decoration: none;
         color: var(--white-color);
         background-color: var(--action-color);
       }
+
       .content {
         display: flex;
         flex-flow: column;
