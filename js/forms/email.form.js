6682e61fd05c33dbb6088a8c4d45ac8f3688acb2
fescii <isfescii@gmail.com>
2024-05-09 14:18:20 +0300

    User dashboard: Entry A19

--- js/forms/email.form.js
+++ js/forms/email.form.js
@@ -69,306 +69,316 @@ export default class FormEmail extends HTMLElement {
 
   getStyles() {
     return /* css */`
-    <style>
-      *,
-      *:after,
-      *:before {
-        box-sizing: border-box !important;
-        font-family: inherit;
-        -webkit-box-sizing: border-box !important;
-      }
+      <style>
+        *,
+        *:after,
+        *:before {
+          box-sizing: border-box !important;
+          font-family: inherit;
+          -webkit-box-sizing: border-box !important;
+        }
 
-      *:focus {
-        outline: inherit !important;
-      }
+        *:focus {
+          outline: inherit !important;
+        }
 
-      *::-webkit-scrollbar {
-        -webkit-appearance: none;
-      }
-
-      h1,
-      h2,
-      h3,
-      h4,
-      h5,
-      h6 {
-        padding: 0;
-        margin: 0;
-        font-family: inherit;
-      }
-
-      p,
-      ul,
-      ol {
-        padding: 0;
-        margin: 0;
-      }
-
-      a {
-        text-decoration: none;
-      }
-
-      :host {
-        font-size: 16px;
-        display: flex;
-        flex-flow: column;
-        gap: 10px;
-        padding: 0;
-        width: 100%;
-      }
-
-      .top {
-        display: flex;
-        flex-flow: column;
-        gap: 5px;
-        padding: 0;
-        width: 100%;
-      }
-
-      .top > h4.title {
-        border-bottom: var(--story-border-mobile);
-        display: flex;
-        align-items: center;
-        color: var(--title-color);
-        font-size: 1.3rem;
-        font-weight: 500;
-        margin: 0;
-        padding: 0 0 6px 0;
-      }
-
-      .top > .desc {
-        margin: 0;
-        padding: 10px 0;
-        color: var(--gray-color);
-        font-size: 1rem;
-        font-family: var(--font-main), sans-serif;
-      }
-
-      form.fields {
-        margin: 0;
-        width: 100%;
-        display: flex;
-        flex-flow: column;
-        justify-content: center;
-        align-items: center;
-        gap: 20px;
-      }
-
-      form.fields > .field {
-        width: 100%;
-        display: flex;
-        flex-flow: column;
-        justify-content: center;
-        align-items: start;
-        gap: 20px;
-      }
-
-      form.fields.center > .field {
-        align-items: center;
-      }
-
-      form.fields .field .input-group {
-        width: 100%;
-        display: flex;
-        flex-flow: column;
-        justify-content: center;
-        align-items: start;
-        color: var(--text-color);
-        gap: 5px;
-        position: relative;
-        transition: border-color 0.3s ease-in-out;
-      }
-
-      form.fields .field.bio .input-group {
-        width: 100%;
-      }
-
-      form.fields .field.bio .input-group.code,
-      form.fields .field.bio .input-group.email {
-        grid-column: 1/3;
-        width: 100%;
-      }
-
-      form.fields .field .input-group > svg {
-        position: absolute;
-        right: 10px;
-        top: 38px;
-        width: 20px;
-        height: 20px;
-      }
-
-      form.fields .field .input-group > svg {
-        display: none;
-      }
-
-      form.fields .field .input-group.success > svg {
-        display: inline-block;
-      }
-
-      form.fields .field .input-group.failed > svg {
-        display: inline-block;
-      }
-
-      form.fields .field .input-group.success > svg {
-        color: var(--accent-color);
-      }
-
-      form.fields .field .input-group.failed > svg {
-        color: var(--error-color);
-      }
-
-      form.fields label {
-        padding: 0 0 5px 0;
-        color: var(--text-color);
-      }
-
-      form.fields .field.bio label {
-        padding: 0 0 0 5px;
-      }
-
-      form.fields label {
-        color: var(--text-color);
-        font-size: 1.1rem;
-        font-family: var(--font-main), sans-serif;
-        transition: all 0.3s ease-in-out;
-        pointer-events: none;
-      }
-
-      form.fields .field input {
-        border: var(--input-border);
-        font-size: 1rem;
-        width: 100%;
-        height: 40px;
-        outline: none;
-        padding: 10px 12px;
-        border-radius: 12px;
-        color: var(--text-color);
-        -webkit-border-radius: 12px;
-        -moz-border-radius: 12px;
-        -ms-border-radius: 12px;
-        -o-border-radius: 12px;
-      }
-
-      form.fields .field input:focus {
-        border: var(--input-border-focus);
-      }
-
-      form.fields .field span.wrapper {
-        display: flex;
-        align-items: center;
-        align-items: center;
-        gap: 0;
-        width: 100%;
-      }
-
-      form.fields .field .input-group.success > span.wrapper > input,
-      form.fields .field .input-group.success > span.wrapper > input:focus,
-      form.fields .field .input-group.success input,
-      form.fields .field .input-group.success input:focus {
-        border: var(--input-border-focus);
-      }
-
-      form.fields .field .input-group.failed > span.wrapper > input,
-      form.fields .field .input-group.failed > span.wrapper > input:focus,
-      form.fields .field .input-group.failed input,
-      form.fields .field .input-group.failed input:focus {
-        border: var(--input-border-error);
-      }
-
-      form.fields .field .input-group.success span.wrapper > input,
-      form.fields .field .input-group.success input {
-        color: var(--accent-color);
-      }
-
-      form.fields .field .input-group.failed span.wrapper > input,
-      form.fields .field .input-group.failed input {
-        color: var(--error-color);
-      }
-
-      form.fields label.focused {
-        top: -10px;
-        font-size: 0.9rem;
-        background-color: var(--label-focus-background);
-        padding: 0 5px;
-      }
-
-      form.fields .field span.status {
-        color: var(--error-color);
-        font-size: 0.95rem;
-        display: none;
-        padding: 0 0 0 5px;
-      }
-
-      form.fields .field .input-group.failed span.status {
-        color: var(--error-color);
-        font-size: 0.8rem;
-        display: inline-block;
-      }
-
-      form.fields .field .input-group.success span.status {
-        color: var(--accent-color);
-        font-size: 0.8rem;
-        display: inline-block;
-      }
-
-      form.fields .field .input-group.success span.status {
-        display: none;
-      }
-
-      form.fields .actions {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        width: 100%;
-        margin: 0 0 0 2px;
-      }
-
-      form.fields .actions > .action {
-        display: flex;
-        flex-flow: row;
-        justify-content: center;
-        align-items: center;
-        gap: 5px;
-        border: none;
-        border-radius: 10px;
-        font-family: var(--font-main), sans-serif;
-        line-height: 1.2;
-        font-size: 1rem;
-        font-weight: 500;
-        color: var(--text-color);
-        width: max-content;
-        padding: 8px 20px;
-        height: 40px;
-        cursor: pointer;
-        position: relative;
-        -webkit-border-radius: 10px;
-        -moz-border-radius: 10px;
-        -ms-border-radius: 10px;
-        -o-border-radius: 10px;
-      }
-
-      form.fields .actions > .action.prev svg path {
-        fill: var(--text-color);
-      }
-
-      form.fields .actions > .action.next {
-        color: var(--white-color);
-        background: var(--stage-no-linear);
-      }
-
-      form.fields .actions > .action.next svg path {
-        fill: var(--white-color);
-      }
-
-      form.fields .actions > .action.disabled {
-        pointer-events: none;
-      }
-      @media screen and (max-width:660px) {
-        ::-webkit-scrollbar {
+        *::-webkit-scrollbar {
           -webkit-appearance: none;
         }
-      }
-    </style>
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+          padding: 0;
+          margin: 0;
+          font-family: inherit;
+        }
+
+        p,
+        ul,
+        ol {
+          padding: 0;
+          margin: 0;
+        }
+
+        a {
+          text-decoration: none;
+        }
+
+        :host {
+          font-size: 16px;
+          display: flex;
+          flex-flow: column;
+          gap: 10px;
+          padding: 0;
+          width: 100%;
+        }
+
+        .top {
+          display: flex;
+          flex-flow: column;
+          gap: 5px;
+          padding: 0;
+          width: 100%;
+        }
+
+        .top > h4.title {
+          border-bottom: var(--story-border-mobile);
+          display: flex;
+          align-items: center;
+          color: var(--title-color);
+          font-size: 1.3rem;
+          font-weight: 500;
+          margin: 0;
+          padding: 0 0 6px 0;
+        }
+
+        .top > .desc {
+          margin: 0;
+          padding: 10px 0;
+          color: var(--gray-color);
+          font-size: 1rem;
+          font-family: var(--font-main), sans-serif;
+        }
+
+        form.fields {
+          margin: 0;
+          width: 100%;
+          display: flex;
+          flex-flow: column;
+          justify-content: center;
+          align-items: center;
+          gap: 20px;
+        }
+
+        form.fields > .field {
+          width: 100%;
+          display: flex;
+          flex-flow: column;
+          justify-content: center;
+          align-items: start;
+          gap: 20px;
+        }
+
+        form.fields.center > .field {
+          align-items: center;
+        }
+
+        form.fields .field .input-group {
+          width: 100%;
+          display: flex;
+          flex-flow: column;
+          justify-content: center;
+          align-items: start;
+          color: var(--text-color);
+          gap: 5px;
+          position: relative;
+          transition: border-color 0.3s ease-in-out;
+        }
+
+        form.fields .field.bio .input-group {
+          width: 100%;
+        }
+
+        form.fields .field.bio .input-group.code,
+        form.fields .field.bio .input-group.email {
+          grid-column: 1/3;
+          width: 100%;
+        }
+
+        form.fields .field .input-group > svg {
+          position: absolute;
+          right: 10px;
+          top: 38px;
+          width: 20px;
+          height: 20px;
+        }
+
+        form.fields .field .input-group > svg {
+          display: none;
+        }
+
+        form.fields .field .input-group.success > svg {
+          display: inline-block;
+        }
+
+        form.fields .field .input-group.failed > svg {
+          display: inline-block;
+        }
+
+        form.fields .field .input-group.success > svg {
+          color: var(--accent-color);
+        }
+
+        form.fields .field .input-group.failed > svg {
+          color: var(--error-color);
+        }
+
+        form.fields label {
+          padding: 0 0 5px 0;
+          color: var(--text-color);
+        }
+
+        form.fields .field.bio label {
+          padding: 0 0 0 5px;
+        }
+
+        form.fields label {
+          color: var(--text-color);
+          font-size: 1.1rem;
+          font-family: var(--font-main), sans-serif;
+          transition: all 0.3s ease-in-out;
+          pointer-events: none;
+        }
+
+        form.fields .field input {
+          border: var(--input-border);
+          font-size: 1rem;
+          width: 100%;
+          height: 40px;
+          outline: none;
+          padding: 10px 12px;
+          border-radius: 12px;
+          color: var(--text-color);
+          -webkit-border-radius: 12px;
+          -moz-border-radius: 12px;
+          -ms-border-radius: 12px;
+          -o-border-radius: 12px;
+        }
+
+        form.fields .field input:focus {
+          border: var(--input-border-focus);
+        }
+
+        form.fields .field span.wrapper {
+          display: flex;
+          align-items: center;
+          align-items: center;
+          gap: 0;
+          width: 100%;
+        }
+
+        form.fields .field .input-group.success > span.wrapper > input,
+        form.fields .field .input-group.success > span.wrapper > input:focus,
+        form.fields .field .input-group.success input,
+        form.fields .field .input-group.success input:focus {
+          border: var(--input-border-focus);
+        }
+
+        form.fields .field .input-group.failed > span.wrapper > input,
+        form.fields .field .input-group.failed > span.wrapper > input:focus,
+        form.fields .field .input-group.failed input,
+        form.fields .field .input-group.failed input:focus {
+          border: var(--input-border-error);
+        }
+
+        form.fields .field .input-group.success span.wrapper > input,
+        form.fields .field .input-group.success input {
+          color: var(--accent-color);
+        }
+
+        form.fields .field .input-group.failed span.wrapper > input,
+        form.fields .field .input-group.failed input {
+          color: var(--error-color);
+        }
+
+        form.fields label.focused {
+          top: -10px;
+          font-size: 0.9rem;
+          background-color: var(--label-focus-background);
+          padding: 0 5px;
+        }
+
+        form.fields .field span.status {
+          color: var(--error-color);
+          font-size: 0.95rem;
+          display: none;
+          padding: 0 0 0 5px;
+        }
+
+        form.fields .field .input-group.failed span.status {
+          color: var(--error-color);
+          font-size: 0.8rem;
+          display: inline-block;
+        }
+
+        form.fields .field .input-group.success span.status {
+          color: var(--accent-color);
+          font-size: 0.8rem;
+          display: inline-block;
+        }
+
+        form.fields .field .input-group.success span.status {
+          display: none;
+        }
+
+        form.fields .actions {
+          display: flex;
+          align-items: center;
+          justify-content: space-between;
+          width: 100%;
+          margin: 0 0 0 2px;
+        }
+
+        form.fields .actions > .action {
+          display: flex;
+          flex-flow: row;
+          justify-content: center;
+          align-items: center;
+          gap: 5px;
+          border: none;
+          border-radius: 10px;
+          font-family: var(--font-main), sans-serif;
+          line-height: 1.2;
+          font-size: 1rem;
+          font-weight: 500;
+          color: var(--text-color);
+          width: max-content;
+          padding: 8px 20px;
+          height: 40px;
+          cursor: pointer;
+          position: relative;
+          -webkit-border-radius: 10px;
+          -moz-border-radius: 10px;
+          -ms-border-radius: 10px;
+          -o-border-radius: 10px;
+        }
+
+        form.fields .actions > .action.prev svg path {
+          fill: var(--text-color);
+        }
+
+        form.fields .actions > .action.next {
+          color: var(--white-color);
+          background: var(--stage-no-linear);
+        }
+
+        form.fields .actions > .action.next svg path {
+          fill: var(--white-color);
+        }
+
+        form.fields .actions > .action.disabled {
+          pointer-events: none;
+        }
+
+        @media screen and (max-width:600px) {
+          ::-webkit-scrollbar {
+            -webkit-appearance: none;
+          }
+
+          .top > .desc {
+            margin: 0;
+            padding: 6px 0 10px;
+            color: var(--gray-color);
+            font-size: 1rem;
+            line-height: 1.5;
+            font-family: var(--font-main), sans-serif;
+          }
+        }
+      </style>
     `;
   }
 }
