8080fbf180829bd7905f4acbdd2a83eaf55d83c7
fescii <isfescii@gmail.com>
2024-05-28 18:02:57 +0300

    Que: Page

--- js/post/quick.post.js
+++ js/post/quick.post.js
@@ -1,4 +1,4 @@
-export default class StoryWrapper extends HTMLElement {
+export default class QuickPost extends HTMLElement {
   constructor() {
     // We are not even going to touch this.
     super();
@@ -16,7 +16,6 @@ export default class StoryWrapper extends HTMLElement {
   connectedCallback() {
     // console.log('We are inside connectedCallback');
 
-    this.upVote();
     this.openForm();
     this.openProfile();
   }
@@ -112,107 +111,6 @@ export default class StoryWrapper extends HTMLElement {
     }
   }
 
-  upVote() {
-    const outerThis = this;
-    let container = this.shadowObj.querySelector(".stats>.stat.upvote");
-    if (container) {
-      let outerNum = container.querySelector(".numb_list");
-      outerNum.scrollBy({
-        top: outerNum.scrollHeight + 10,
-        behavior: "smooth"
-      })
-
-      container.addEventListener('click', (event) => {
-        event.preventDefault();
-
-        const upvoted = outerThis.getAttribute("upvoted") || "false";
-        // console.log(upvoted);
-
-        let numb = container.querySelector(".numb_list");
-        let num = numb.querySelector(`#u-${outerThis.getAttribute('upvotes')}`);
-        // console.log(num);
-        let numHolder = num.textContent;
-
-        if (upvoted === "false") {
-          container.classList.add("active");
-          try {
-            numHolder = parseInt(numHolder) + 1;
-          }
-          catch (error) {
-            console.error(error);
-          }
-
-          let newNum = document.createElement("span");
-          newNum.setAttribute("id", `u-${numHolder}`)
-          newNum.innerText = numHolder;
-
-          outerThis.setAttribute("upvotes", numHolder);
-          outerThis.setAttribute("upvoted", 'true');
-
-          numb.appendChild(newNum);
-          numb.scrollBy({
-            top: num.scrollHeight + 100,
-            behavior: "smooth"
-          })
-        }
-        else {
-          container.classList.remove("active");
-          container.classList.remove("true");
-
-          try {
-            numHolder = parseInt(numHolder) - 1;
-
-            if (typeof numHolder === "number") {
-              if (numHolder < 1) {
-                numHolder = 0
-              }
-            }
-          }
-          catch (error) {
-            console.error(error);
-          }
-
-          let allNum = numb.querySelectorAll('span');
-          let removeNum = numb.querySelector(`#u-${outerThis.getAttribute('upvotes')}`);
-
-          if (allNum.length > 1) {
-            removeNum.remove()
-          }
-          else {
-            removeNum.textContent = numHolder;
-            removeNum.setAttribute("id", `u-${numHolder}`)
-          }
-          outerThis.setAttribute("upvotes", numHolder);
-          outerThis.setAttribute("upvoted", 'false');
-
-          numb.scrollTo({
-            top: 0,
-            behavior: "smooth"
-          })
-        }
-      })
-    }
-  }
-
-  expandTextArea = () => {
-    const replyTextarea = this.shadowObj.querySelector('textarea');
-    if (replyTextarea) {
-      replyTextarea.addEventListener('input', () => {
-        replyTextarea.style.height = 'auto';
-        const height = replyTextarea.scrollHeight;
-        if (height <= 60) {
-          replyTextarea.style.height = `38px`;
-        }
-        else if (height <= 100) {
-          replyTextarea.style.height = `${height + 10}px`;
-        }
-        else {
-          replyTextarea.style.height = `100px`;
-        }
-      });
-    }
-  }
-
   getTemplate() {
     // Show HTML Here
     return `
@@ -221,54 +119,33 @@ export default class StoryWrapper extends HTMLElement {
     `;
   }
 
-  getOpinionContent = () => {
-    const contents = this.innerHTML.split('\n');
-
-    let output = ``
-    contents.forEach(content => {
-      let trimContent = content.trim();
-      if (trimContent !== '' && trimContent.length >= 1) {
-        // console.log(trimContent);
-        output += `<p>${trimContent}</p>`
-      }
-    })
-
+  getBody() {
     return `
-      <div class="content">
-        ${output}
-      </div>
+      ${this.getHeader()}
+      ${this.getContent()}
+      ${this.getFooter()}
+      <div class="form-container"></div>
     `;
   }
 
-  getHeader = (story) => {
-    if (story === "opinion") {
-      return `
-        <div class="meta opinion">
-          <span class="time">
-            <time class="published" datetime="${this.getAttribute('time')}">
-              ${this.formatDateWithRelativeTime(this.getAttribute('time'))}
-            </time>
-            <span class="sp">•</span>
-          </span>
-          <div class="author">
-            <span class="sp">by</span>
-            <div class="author-name">
-              <a href="" class="link action-link">${this.getAttribute('author-id')}</a>
-              ${this.getAuthor()}
-            </div>
+  getHeader = () => {
+    return /*html*/`
+      <div class="meta opinion">
+        <span class="time">
+          <time class="published" datetime="${this.getAttribute('time')}">
+            ${this.formatDateWithRelativeTime(this.getAttribute('time'))}
+          </time>
+          <span class="sp">•</span>
+        </span>
+        <div class="author">
+          <span class="sp">by</span>
+          <div class="author-name">
+            <a href="" class="link action-link">${this.getAttribute('author-id')}</a>
+            ${this.getAuthor()}
           </div>
         </div>
-      `
-    }
-    else {
-      return `
-        <span class="read-time">
-          <span class="text">${this.getAttribute('read-time')} min read</span>
-          <span class="sp">•</span>
-          <span class="views">${this.getAttribute('views')} views</span>
-        </span>
-      `
-    }
+      </div>
+    `
   }
 
   getAuthor = () => {
@@ -330,66 +207,40 @@ export default class StoryWrapper extends HTMLElement {
     }
   }
 
-  getContent = (story) => {
-    if (story === "opinion") {
-      return this.getOpinionContent();
-    }
-    else {
-      return `
-			  <h3 class="title">
-          <a href="" class="link">${this.getAttribute('story-title')}</a>
-        </h3>
-			`;
-    }
+  getContent = () => {
+    return `
+      <div class="content">
+        ${this.innerHTML}
+      </div>
+    `
   }
 
-  getFooter = (story) => {
-    if (story === 'opinion') {
-      return /* html */`
-        <div class="stats">
-          <span class="stat write">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M12 22.0001C4.617 22.0001 2 19.3831 2 12.0001C2 4.61712 4.617 2.00012 12 2.00012C12.414 2.00012 12.75 2.33612 12.75 2.75012C12.75 3.16412 12.414 3.50012 12 3.50012C5.486 3.50012 3.5 5.48612 3.5 12.0001C3.5 18.5141 5.486 20.5001 12 20.5001C18.514 20.5001 20.5 18.5141 20.5 12.0001C20.5 11.5861 20.836 11.2501 21.25 11.2501C21.664 11.2501 22 11.5861 22 12.0001C22 19.3831 19.383 22.0001 12 22.0001Z" fill="currentColor"/>
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M19.2365 9.38606L20.2952 8.19072C21.4472 6.88972 21.3252 4.89472 20.0252 3.74172C19.3952 3.18372 18.5812 2.90372 17.7452 2.95572C16.9052 3.00672 16.1352 3.38272 15.5772 4.01272L9.6932 10.6607C7.8692 12.7187 9.1172 15.4397 9.1712 15.5547C9.2602 15.7437 9.4242 15.8877 9.6232 15.9497C9.6802 15.9687 10.3442 16.1717 11.2192 16.1717C12.2042 16.1717 13.4572 15.9127 14.4092 14.8367L19.0774 9.56571C19.1082 9.54045 19.1374 9.51238 19.1646 9.4815C19.1915 9.45118 19.2155 9.41925 19.2365 9.38606ZM10.4082 14.5957C11.0352 14.7097 12.4192 14.8217 13.2862 13.8427L17.5371 9.04299L15.0656 6.85411L10.8172 11.6557C9.9292 12.6567 10.2122 13.9917 10.4082 14.5957ZM16.0596 5.73076L18.5322 7.91938L19.1722 7.19672C19.7752 6.51472 19.7122 5.46872 19.0312 4.86572C18.7002 4.57372 18.2712 4.42472 17.8362 4.45272C17.3962 4.48072 16.9932 4.67672 16.7002 5.00672L16.0596 5.73076Z" fill="currentColor"/>
-            </svg>
-            <span class="line"></span>
+  getFooter = () => {
+    return /* html */`
+      <div class="stats">
+        <span class="stat write">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 22.0001C4.617 22.0001 2 19.3831 2 12.0001C2 4.61712 4.617 2.00012 12 2.00012C12.414 2.00012 12.75 2.33612 12.75 2.75012C12.75 3.16412 12.414 3.50012 12 3.50012C5.486 3.50012 3.5 5.48612 3.5 12.0001C3.5 18.5141 5.486 20.5001 12 20.5001C18.514 20.5001 20.5 18.5141 20.5 12.0001C20.5 11.5861 20.836 11.2501 21.25 11.2501C21.664 11.2501 22 11.5861 22 12.0001C22 19.3831 19.383 22.0001 12 22.0001Z" fill="currentColor"/>
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M19.2365 9.38606L20.2952 8.19072C21.4472 6.88972 21.3252 4.89472 20.0252 3.74172C19.3952 3.18372 18.5812 2.90372 17.7452 2.95572C16.9052 3.00672 16.1352 3.38272 15.5772 4.01272L9.6932 10.6607C7.8692 12.7187 9.1172 15.4397 9.1712 15.5547C9.2602 15.7437 9.4242 15.8877 9.6232 15.9497C9.6802 15.9687 10.3442 16.1717 11.2192 16.1717C12.2042 16.1717 13.4572 15.9127 14.4092 14.8367L19.0774 9.56571C19.1082 9.54045 19.1374 9.51238 19.1646 9.4815C19.1915 9.45118 19.2155 9.41925 19.2365 9.38606ZM10.4082 14.5957C11.0352 14.7097 12.4192 14.8217 13.2862 13.8427L17.5371 9.04299L15.0656 6.85411L10.8172 11.6557C9.9292 12.6567 10.2122 13.9917 10.4082 14.5957ZM16.0596 5.73076L18.5322 7.91938L19.1722 7.19672C19.7752 6.51472 19.7122 5.46872 19.0312 4.86572C18.7002 4.57372 18.2712 4.42472 17.8362 4.45272C17.3962 4.48072 16.9932 4.67672 16.7002 5.00672L16.0596 5.73076Z" fill="currentColor"/>
+          </svg>
+          <span class="line"></span>
+        </span>
+        <span class="stat discuss">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
+          </svg>
+          <span class="no">98</span>
+        </span>
+        <span class="stat upvote ${this.getAttribute('upvoted')}">
+          <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 384 512">
+            <path d="M209.4 39.4C204.8 34.7 198.6 32 192 32s-12.8 2.7-17.4 7.4l-168 176c-9.2 9.6-8.8 24.8 .8 33.9s24.8 8.8 33.9-.8L168 115.9V456c0 13.3 10.7 24 24 24s24-10.7 24-24V115.9L342.6 248.6c9.2 9.6 24.3 9.9 33.9 .8s9.9-24.3 .8-33.9l-168-176z"/>
+          </svg>
+          <span class="numb_list no">
+            <span id="u-${this.getAttribute('upvotes')}">${this.getAttribute('upvotes')}</span>
           </span>
-          <span class="stat discuss">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-              <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"/>
-            </svg>
-            <span class="no">98</span>
-          </span>
-          <span class="stat upvote ${this.getAttribute('upvoted')}">
-            <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" viewBox="0 0 384 512">
-              <path d="M209.4 39.4C204.8 34.7 198.6 32 192 32s-12.8 2.7-17.4 7.4l-168 176c-9.2 9.6-8.8 24.8 .8 33.9s24.8 8.8 33.9-.8L168 115.9V456c0 13.3 10.7 24 24 24s24-10.7 24-24V115.9L342.6 248.6c9.2 9.6 24.3 9.9 33.9 .8s9.9-24.3 .8-33.9l-168-176z"/>
-            </svg>
-            <span class="numb_list no">
-              <span id="u-${this.getAttribute('upvotes')}">${this.getAttribute('upvotes')}</span>
-            </span>
-          </span>
-        </div>
-			`
-    }
-    else {
-      return `
-			  <div class="meta">
-          <div class="author">
-            <span class="sp">by</span>
-            <div class="author-name">
-              <a href="" class="link action-link">${this.getAttribute('author-id')}</a>
-              ${this.getAuthor()}
-            </div>
-          </div>
-          <span class="time">
-            <span class="sp">•</span>
-            <time class="published" datetime="${this.getAttribute('time')}">
-              ${this.formatDateWithRelativeTime(this.getAttribute('time'))}
-            </time>
-          </span>
-        </div>
-			`
-    }
+        </span>
+      </div>
+		`
   }
 
   getForm = () => {
@@ -398,27 +249,11 @@ export default class StoryWrapper extends HTMLElement {
     `
   }
 
-  getBody() {
-    return `
-      ${this.getHeader(this.getAttribute('story'))}
-      ${this.getContent(this.getAttribute('story'))}
-      ${this.getFooter(this.getAttribute('story'))}
-      <div class="form-container"></div>
-    `;
-  }
-
-  checkType = () => {
-    if (this.getAttribute('story') === 'opinion') {
-      return 'padding: 15px 0 0 0;';
-    }
-    else {
-      return 'padding: 15px 0;';
-    }
-  }
 
   getStyles() {
     return /* css */`
     <style>
+
       *,
       *:after,
       *:before {
@@ -462,7 +297,7 @@ export default class StoryWrapper extends HTMLElement {
         font-size: 16px;
         border-bottom: var(--story-border);
         font-family: var(--font-main), sans-serif;
-        ${this.checkType()}
+        padding: 15px 0 0 0;
         margin: 0;
         width: 100%;
         display: flex;
