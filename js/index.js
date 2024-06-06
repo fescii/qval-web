c24efb63ac0d73f073c9f0661aa21cbb16ff7cdc
fescii <isfescii@gmail.com>
2024-06-06 19:07:27 +0300

    refactor: Update padding and gap in profile wrapper for user info section

---
+++ js/index.js
@@ -0,0 +1,171 @@
+// Import apps
+import AppHome from "./apps/home.app.js";
+import AppPost from "./apps/post.app.js";
+import AppProfile from "./apps/profile.app.js";
+import AppSearch from "./apps/search.app.js";
+import AppStory from "./apps/story.app.js";
+import AppTopic from "./apps/topic.app.js";
+import AppUser from "./apps/user.app.js";
+
+// Import Containers
+import AddContainer from "./containers/add.container.js";
+import ActivityContainer from "./containers/activity.container.js";
+import FormContainer from "./containers/form.container.js";
+import HighlightsContainer from "./containers/highlights.container.js";
+import InfoContainer from "./containers/info.container.js";
+import LogonContainer from "./containers/logon.container.js";
+import PeopleContainer from "./containers/people.container.js";
+import PeopleDiscover from "./containers/people.discover.js";
+import RelatedContainer from "./containers/related.container.js";
+import StatContainer from "./containers/stat.container.js";
+import StoriesContainer from "./containers/stories.container.js";
+import TopicsContainer from "./containers/topics.container.js";
+import TrendingContainer from "./containers/trending.container.js";
+
+// Import custom elements
+import CustomSpan from "./elements/span.element.js";
+
+
+// Import posts
+import QuickPost from "./post/quick.post.js";
+import PollPoll from "./post/poll.post.js";
+import StoryPost from "./post/story.post.js";
+
+
+// Import feeds
+import ActivityFeed from "./feeds/activity.feed.js";
+import PeopleFeed from "./feeds/people.feed.js";
+import StoriesFeed from "./feeds/stories.feed.js";
+import RepliesFeed from "./feeds/replies.feed.js";
+import StatFeed from "./feeds/stat.feed.js";
+
+
+// Import forms
+import BioForm from "./forms/bio.form.js";
+import EmailForm from "./forms/email.form.js";
+import PasswordForm from "./forms/password.form.js";
+import ProfileForm from "./forms/profile.form.js";
+import NameForm from "./forms/name.form.js";
+import SocialForm from "./forms/social.form.js";
+
+// Import Loaders
+import AuthorsLoader from "./loaders/authors.loader.js";
+import InfoLoader from "./loaders/info.loader.js";
+import PeopleLoader from "./loaders/people.loader.js";
+import PostLoader from "./loaders/post.loader.js";
+import StoryLoader from "./loaders/story.loader.js";
+import TopicsLoader from "./loaders/topics.loader.js";
+
+
+// Import Sections
+import PostSection from "./sections/post.section.js";
+import ProfileSection from "./sections/profile.section.js";
+
+// Import stats
+import ActivityItem from "./stats/activity.item.js";
+import AllStat from "./stats/all.stat.js";
+import OpinionsStat from "./stats/opinions.stat.js";
+import StatOpinion from "./stats/stat.opinion.js";
+import StatStory from "./stats/stat.story.js";
+import StoriesStat from "./stats/stories.stat.js";
+import UsersStat from "./stats/users.stat.js";
+
+// Import wrappers
+import AuthorWrapper from "./wrappers/author.wrapper.js";
+import PostWrapper from "./wrappers/post.wrapper.js";
+import HeaderWrapper from "./wrappers/header.wrapper.js";
+import PersonWrapper from "./wrappers/person.wrapper.js";
+import PollWrapper from "./wrappers/poll.wrapper.js";
+import ProfileWrapper from "./wrappers/profile.wrapper.js";
+import ShareWrapper from "./wrappers/share.wrapper.js";
+import UserWrapper from "./wrappers/user.wrapper.js";
+
+// Import bodies
+import StoryBody from "./bodies/story.body.js";
+
+
+const initApp = () => {
+
+  // Register apps
+  customElements.define("app-home", AppHome);
+  customElements.define("app-post", AppPost);
+  customElements.define("app-profile", AppProfile);
+  customElements.define("app-search", AppSearch);
+  customElements.define("app-story", AppStory);
+  customElements.define("app-topic", AppTopic);
+  customElements.define("app-user", AppUser);
+
+  // Register containers
+  customElements.define("add-container", AddContainer);
+  customElements.define("activity-container", ActivityContainer);
+  customElements.define("form-container", FormContainer);
+  customElements.define("highlights-container", HighlightsContainer);
+  customElements.define("info-container", InfoContainer);
+  customElements.define("logon-container", LogonContainer);
+  customElements.define("people-container", PeopleContainer);
+  customElements.define("people-discover", PeopleDiscover);
+  customElements.define("related-container", RelatedContainer);
+  customElements.define("stat-container", StatContainer);
+  customElements.define("stories-container", StoriesContainer);
+  customElements.define("topics-container", TopicsContainer);
+  customElements.define("trending-container", TrendingContainer);
+
+  // Register custom elements
+  customElements.define("custom-span", CustomSpan, { extends: "span"});
+
+  // Register posts
+  customElements.define("quick-post", QuickPost);
+  customElements.define("poll-post", PollPoll);
+  customElements.define("story-post", StoryPost);
+
+  // Register feeds
+  customElements.define("activity-feed", ActivityFeed);
+  customElements.define("people-feed", PeopleFeed);
+  customElements.define("stories-feed", StoriesFeed);
+  customElements.define("replies-feed", RepliesFeed);
+  customElements.define("stat-feed", StatFeed);
+
+  // Register forms
+  customElements.define("bio-form", BioForm);
+  customElements.define("email-form", EmailForm);
+  customElements.define("password-form", PasswordForm);
+  customElements.define("profile-form", ProfileForm);
+  customElements.define("name-form", NameForm);
+  customElements.define("social-form", SocialForm);
+
+  // Register loaders
+  customElements.define("authors-loader", AuthorsLoader);
+  customElements.define("info-loader", InfoLoader);
+  customElements.define("people-loader", PeopleLoader);
+  customElements.define("post-loader", PostLoader);
+  customElements.define("story-loader", StoryLoader);
+  customElements.define("topics-loader", TopicsLoader);
+
+  // Register sections
+  customElements.define("post-section", PostSection);
+  customElements.define("profile-section", ProfileSection);
+
+  // Register stats
+  customElements.define("activity-item", ActivityItem);
+  customElements.define("all-stat", AllStat);
+  customElements.define("opinions-stat", OpinionsStat);
+  customElements.define("stat-opinion", StatOpinion);
+  customElements.define("stat-story", StatStory);
+  customElements.define("stories-stat", StoriesStat);
+  customElements.define("users-stat", UsersStat);
+
+  // Register wrappers
+  customElements.define("author-wrapper", AuthorWrapper);
+  customElements.define("post-wrapper", PostWrapper);
+  customElements.define("header-wrapper", HeaderWrapper);
+  customElements.define("person-wrapper", PersonWrapper);
+  customElements.define("poll-wrapper", PollWrapper);
+  customElements.define("profile-wrapper", ProfileWrapper);
+  customElements.define("share-wrapper", ShareWrapper);
+  customElements.define("user-wrapper", UserWrapper);
+
+  // Register bodies
+  customElements.define("story-body", StoryBody);
+}
+
+export default initApp;
